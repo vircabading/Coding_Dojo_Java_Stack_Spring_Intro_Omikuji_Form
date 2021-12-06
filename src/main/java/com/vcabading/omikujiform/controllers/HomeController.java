e4152488faa9dc771e3340d29c8a5a71d251c253
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-05 16:39:03 -0800

    add static/css/style.css and static/js

--- src/main/java/com/vcabading/omikujiform/controllers/HomeController.java
+++ src/main/java/com/vcabading/omikujiform/controllers/HomeController.java
@@ -1,3 +1,5 @@
+package com.vcabading.omikujiform.controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
