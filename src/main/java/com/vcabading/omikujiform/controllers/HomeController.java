ba0ad5a819d80121cc5adb40ce7f17ae0dd649c3
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-05 16:24:47 -0800

    add home controller

---
+++ src/main/java/com/vcabading/omikujiform/controllers/HomeController.java
@@ -0,0 +1,21 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/////////////////////////////////////////////////////////////////
+//	HOME CONTROLLER
+/////////////////////////////////////////////////////////////////
+
+@Controller
+public class HomeController {
+	
+	//	//// GET ////////////////////////////////////////////////
+	
+    @GetMapping("/")
+    public String index(Model model) {
+    	model.addAttribute("dojoName", "San Jose");
+        return "index.jsp";
+    }
+    
+    //	//// POST ///////////////////////////////////////////////
+}
